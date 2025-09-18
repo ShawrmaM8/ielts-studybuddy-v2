1bb92c44f994093eb3c817fd4ad6b5d86afdc87f
Winorrepeat <155412365+ShawrmaM8@users.noreply.github.com>
2025-09-18 05:16:25 +0300

    Delete lang_feedback.py

--- lang_feedback.py
+++
@@ -1,9 +0,0 @@
-from language_tool_python import LanguageTool
-
-def get_language_feedback(text):
-    try:
-        tool = LanguageTool('en-US')  # Local
-    except:
-        tool = LanguageTool('en-US', remote=True)  # Fallback
-    matches = tool.check(text)
-    return [{'rule': m.ruleId, 'message': m.message, 'context': m.context} for m in matches]
